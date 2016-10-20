999c52d062155d7775cecab1cf845a93b48585d7
razvan-tudosa <razvan.tudosa@thinslices.com>
2016-10-20 17:34:05 +0300

    Checkpoint
    - added loader for the card
    - debounced the `mouseover` event listener
    - minor tweaks here and there

--- app/scripts.babel/content.js
+++ app/scripts.babel/content.js
@@ -1,20 +1,42 @@
 console.log('Mighty content js speaks to us... blaaa!');
 
+const spinner = {
+  domRef: null,
+  init: (parent) => {
+    spinner.domRef = document.createElement('div');
+    spinner.domRef.classList.add('custom-spinner');
+
+    let item1 = document.createElement('div');
+    item1.classList.add('double-bounce1');
+    spinner.domRef.appendChild(item1);
+
+    let item2 = document.createElement('div');
+    item2.classList.add('double-bounce2');
+    spinner.domRef.appendChild(item2);
+
+    parent.appendChild(spinner.domRef);
+  }
+};
+
 const api = {
   card: {
     domRef: null,
     content: {}
   },
   handlers: {
+    helpers: {
+      tdebouncedMouseOver: null
+    },
     mouseOver: (e) => {
       let link = e.target.parentNode.href;
       let imdbId = link.match(/nm[0-9]*/g)[0];
 
       const position = {
-        top: e.pageY - 200 - 15 + 'px',
+        top: e.pageY - 220 - 15 + 'px',
         left: e.pageX + 'px'
       };
 
+      api.clearCard();
       api.showCard(position);
 
       api.getActorData(imdbId)
@@ -24,12 +46,14 @@ const api = {
         });
     },
     mouseOut: () => {
+      api.handlers.helpers.tdebouncedMouseOver.cancel();
       api.hideCard();
     }
   },
   createCard: () => {
     api.card.domRef = document.createElement('div');
     api.card.domRef.classList.add('helper-card');
+    api.card.domRef.classList.add('is-loading');
     api.card.content.image = document.createElement('img');
     api.card.domRef.appendChild(api.card.content.image);
     document.querySelector('body').appendChild(api.card.domRef);
@@ -44,30 +68,39 @@ const api = {
   },
   populateCard: (actorInfo) => {
     api.card.content.image.src = actorInfo.image;
+    api.card.domRef.classList.remove('is-loading');
   },
   showCard: (position) => {
     api.card.domRef.classList.add('show');
     api.card.domRef.style.top = position.top;
     api.card.domRef.style.left = position.left;
   },
+  clearCard: () => {
+    api.card.domRef.classList.add('is-loading');
+  },
   hideCard: () => {
     api.card.domRef.classList.remove('show');
-    api.card.content.image.src = '';
+    api.clearCard();
   },
   _attachEventHandlers: () => {
     var actorsDomElements = document.querySelectorAll('.cast_list .itemprop a');
 
     const THROTTLE_TIME = 500;
 
+    api.handlers.helpers.tdebouncedMouseOver = _.debounce(api.handlers.mouseOver, THROTTLE_TIME);
+
     [].forEach.call(actorsDomElements, function(actor) {
-      actor.addEventListener('mouseover', _.throttle(api.handlers.mouseOver, THROTTLE_TIME));
-      actor.addEventListener('mouseout', _.throttle(api.handlers.mouseOut, THROTTLE_TIME));
+      actor.addEventListener('mouseover', api.handlers.helpers.tdebouncedMouseOver);
+      actor.addEventListener('mouseout', api.handlers.mouseOut);
     });
   },
   init: () => {
     api.createCard();
+    spinner.init(api.card.domRef);
     api._attachEventHandlers();
   }
 };
+
+
 
 api.init();
--- app/scripts/content.js
+++ app/scripts/content.js
@@ -2,21 +2,43 @@
 
 console.log('Mighty content js speaks to us... blaaa!');
 
+var spinner = {
+  domRef: null,
+  init: function init(parent) {
+    spinner.domRef = document.createElement('div');
+    spinner.domRef.classList.add('custom-spinner');
+
+    var item1 = document.createElement('div');
+    item1.classList.add('double-bounce1');
+    spinner.domRef.appendChild(item1);
+
+    var item2 = document.createElement('div');
+    item2.classList.add('double-bounce2');
+    spinner.domRef.appendChild(item2);
+
+    parent.appendChild(spinner.domRef);
+  }
+};
+
 var api = {
   card: {
     domRef: null,
     content: {}
   },
   handlers: {
+    helpers: {
+      tdebouncedMouseOver: null
+    },
     mouseOver: function mouseOver(e) {
       var link = e.target.parentNode.href;
       var imdbId = link.match(/nm[0-9]*/g)[0];
 
       var position = {
-        top: e.pageY - 200 - 15 + 'px',
+        top: e.pageY - 220 - 15 + 'px',
         left: e.pageX + 'px'
       };
 
+      api.clearCard();
       api.showCard(position);
 
       api.getActorData(imdbId).then(function (response) {
@@ -25,12 +47,14 @@ var api = {
       });
     },
     mouseOut: function mouseOut() {
+      api.handlers.helpers.tdebouncedMouseOver.cancel();
       api.hideCard();
     }
   },
   createCard: function createCard() {
     api.card.domRef = document.createElement('div');
     api.card.domRef.classList.add('helper-card');
+    api.card.domRef.classList.add('is-loading');
     api.card.content.image = document.createElement('img');
     api.card.domRef.appendChild(api.card.content.image);
     document.querySelector('body').appendChild(api.card.domRef);
@@ -44,28 +68,35 @@ var api = {
   },
   populateCard: function populateCard(actorInfo) {
     api.card.content.image.src = actorInfo.image;
+    api.card.domRef.classList.remove('is-loading');
   },
   showCard: function showCard(position) {
     api.card.domRef.classList.add('show');
     api.card.domRef.style.top = position.top;
     api.card.domRef.style.left = position.left;
   },
+  clearCard: function clearCard() {
+    api.card.domRef.classList.add('is-loading');
+  },
   hideCard: function hideCard() {
     api.card.domRef.classList.remove('show');
-    api.card.content.image.src = '';
+    api.clearCard();
   },
   _attachEventHandlers: function _attachEventHandlers() {
     var actorsDomElements = document.querySelectorAll('.cast_list .itemprop a');
 
     var THROTTLE_TIME = 500;
 
+    api.handlers.helpers.tdebouncedMouseOver = _.debounce(api.handlers.mouseOver, THROTTLE_TIME);
+
     [].forEach.call(actorsDomElements, function (actor) {
-      actor.addEventListener('mouseover', _.throttle(api.handlers.mouseOver, THROTTLE_TIME));
-      actor.addEventListener('mouseout', _.throttle(api.handlers.mouseOut, THROTTLE_TIME));
+      actor.addEventListener('mouseover', api.handlers.helpers.tdebouncedMouseOver);
+      actor.addEventListener('mouseout', api.handlers.mouseOut);
     });
   },
   init: function init() {
     api.createCard();
+    spinner.init(api.card.domRef);
     api._attachEventHandlers();
   }
 };
